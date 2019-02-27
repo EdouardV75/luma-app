c5bdadc928189146af4ae0be3dc6ebb9430b2d6a
DylanAdam <adamdylan.p@gmail.com>
2019-02-27 17:05:45 +0100

    modified home and searchbar + navbar

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,22 @@
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-wagon');
+  const whiteLogo = document.getElementById('white_logo');
+  const blueLogo = document.getElementById('blue_logo')
+  const searchbar = document.getElementById('searchbarhome')
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-wagon-white');
+        whiteLogo.classList.add('hidden');
+        blueLogo.classList.remove('hidden');
+      } else {
+        navbar.classList.remove('navbar-wagon-white');
+        whiteLogo.classList.remove('hidden')
+        blueLogo.classList.add('hidden')
+        
+      }
+    });
+  }
+};
+
+export { initUpdateNavbarOnScroll };
