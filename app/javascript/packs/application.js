179bf6feb9aadfdb9d2d6b4fc0eb442728ba7090
claire demont <claire.edith.demont@gmail.com>
2019-02-28 17:31:41 +0100

    index avec category dynamique

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,10 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner.js';
 import { homeWrapper } from '../components/homeWrapper.js';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { findCategory } from '../components/index';
+
 
 initUpdateNavbarOnScroll();
 loadDynamicBannerText();
 homeWrapper();
-
-
+findCategory();
