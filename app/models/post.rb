003d758f0b8903989ece4a241670aa32641fdf0d
claire demont <claire.edith.demont@gmail.com>
2019-03-01 14:37:59 +0100

    add dependant destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :post_categories
+  has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
 end
