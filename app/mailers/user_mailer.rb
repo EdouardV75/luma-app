d3b658f83008e1fdd4fd9d589c2cd4c3515a7f37
DylanAdam <adamdylan.p@gmail.com>
2019-03-04 16:50:01 +0100

    modified the card index

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
     @user = user # Instance variable => available in view
     @url = root_url
 
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: 'Welcome to Luma')
     # This will render a view in `app/views/user_mailer`!
   end
 end
