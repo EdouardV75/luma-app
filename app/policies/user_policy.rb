b760ade72115b5425f11d7a37bde6975dbcdd624
DylanAdam <adamdylan.p@gmail.com>
2019-03-05 17:34:59 +0100

    added the final page

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,9 @@
-
-  class UserPolicy < ApplicationPolicy
-
+class UserPolicy < ApplicationPolicy
     def dashboard?
       user.office_manager?
+    end
+
+    def confirmed?
+      true
     end
 end
